6355a42983fd9fa28c8f6e903408f4322bd217a9
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-30 15:51:41 +0530

    started

---
+++ file_transfer.sh
@@ -0,0 +1,15 @@
+file=hello.html
+bucket=your-bucket
+resource="/${bucket}/${file}"
+contentType="text/html"
+dateValue=`30/04/2020`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=" "
+s3Secret=" "
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${file}
