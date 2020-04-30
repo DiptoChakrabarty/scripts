2eca5114303cae865d415dad55828ddda7eba1b6
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-30 16:15:49 +0530

    kube

--- python.sh
+++ python.sh
@@ -1,3 +1,5 @@
-scp  app.py   <ip-addr>:/app.py
-scp  requirements.txt   <ip-addr>:/requirements.txt
-scp  Dockerfile   <ip-addr>:/Dockerfile
+scp  app.py   user@<ip-addr>:/app.py
+scp  requirements.txt   user@<ip-addr>:/requirements.txt
+scp  Dockerfile   user@<ip-addr>:/Dockerfile
+
+ssh user@<ip-addr>  'docker build -t sample .'
