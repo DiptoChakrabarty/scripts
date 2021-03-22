464e9b8ff82bad7f2ca218ad4bf992f1c4586b9d
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2021-03-22 19:16:58 +0530

    script change

--- python.sh
+++ python.sh
@@ -1,5 +1,5 @@
-scp  app.py   user@<ip-addr>:/app.py
+scp  app.py   user@{ip-addr}:/app.py
 scp  requirements.txt   user@<ip-addr>:/requirements.txt
-scp  Dockerfile   user@<ip-addr>:/Dockerfile
+scp  Dockerfile   user@{ip-addr}:/Dockerfile
 
-ssh user@<ip-addr>  'docker build -t sample .'
+ssh user@{ip-addr}  'docker build -t sample .'
